337a1d7726b3a18156f8c561825f0cce5643691a
Jeremy Reikes <jeremyreikes@gmail.com>
2020-04-17 17:22:36 -0400

    removed old code

--- playlist_recommendation.py
+++ playlist_recommendation.py
@@ -6,47 +6,6 @@ playlists_db = db.playlists_db
 import pandas as pd
 from collections import Counter
 
-
-def get_playlist_word_frequencies(tid, target_word=None):
-    word_counts = Counter()
-    playlist_ids = get_track_playlists(tid)
-    total_occurences = len(playlist_ids)
-    for playlist_id in playlist_ids:
-        lemmas = get_playlist_lemmas(playlist_id)
-        for lemma in lemmas:
-            word_counts[lemma] += 1
-    for word in word_counts:
-        word_counts[word] /= total_occurences
-    if target_word:
-        return word_counts.get(target_word, 0)
-    return word_counts
-
-def get_frequencies_for_word(word):
-    all_tids = list()
-    playlist_occurences = Counter()
-    playlists = playlists_db.find({'lemmas': word})
-    for playlist in playlists:
-        tids = set(playlist['tids'])
-        for tid in tids:
-            playlist_occurences[tid] += 1
-        all_tids.extend(tids)
-    all_tids = list(set(all_tids))
-
-    total_word_playlists = playlists.count()
-    all_playlist_frequencies = dict()
-    word_playlist_frequencies = dict()
-    track_occurences = dict()
-
-    for track in tracks_db.find({'_id': {'$in': all_tids}}, {'_id': 1, 'playlists': 1}):
-        tid = track['_id']
-        total_track_playlists = len(track['playlists'])
-
-        if total_track_playlists > 10:
-            all_playlist_frequencies[tid] = playlist_occurences[tid] / total_track_playlists
-        word_playlist_frequencies[tid] = playlist_occurences[tid] / total_word_playlists
-
-    return word_playlist_frequencies, all_playlist_frequencies
-
 def track_frequency(word):
     frequencies = Counter()
     playlists = playlists_db.find({'name_lemmas': word})
@@ -56,7 +15,6 @@ def track_frequency(word):
         for tid in tids:
             frequencies[tid] += 1
     return frequencies
-
 
 # if song occurs in less than min_occurences playlists, don't consider it.
 def weighted_track_frequency(word, min_occurences=None):
