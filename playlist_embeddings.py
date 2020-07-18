9670ee8d2b45a9f78adb4f643f1ba5bca25830b5
Jeremy Reikes <jeremyreikes@gmail.com>
2020-07-18 19:19:46 -0400

    deleted comments

--- playlist_embeddings.py
+++ playlist_embeddings.py
@@ -26,16 +26,3 @@ def playlist_addition_recommendations(pid, n=10):
     tids = db.get_playlist_tids(pid)
     track_recs = get_similar_tracks(tids)
     return track_recs
-
-model = create_model()
-'''
-Death Bed (coffee for your head) - Powfu
-10 most similar songs based on song embeddings
-'''
-# tid = '24Yi9hE78yPEbZ4kxyoXAI'
-# similar_tracks = get_similar_tracks(tid)
-# similar_tracks
-#
-# pid = '3e938S67Zucv2YVYGH058k'
-# playlist_addition_recs = playlist_addition_recommendations(pid)
-# playlist_addition_recs
